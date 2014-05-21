49f99dd6c3d6c5375ba54d6f1cc8760a8088dd4a
Magnus Eriksson <mange@reloop.se>
2014-05-22 01:06:23 +0200

    Added getAccessToken(). Closes #2

--- src/Maer/GoogleAuth/GoogleAuth.php
+++ src/Maer/GoogleAuth/GoogleAuth.php
@@ -3,12 +3,11 @@
 use App;
 use Config;
 use Input;
-use League\OAuth2\Client\Token\AccessToken;
 
 class GoogleAuth
 {
     private $provider;
-    private $accessTokenObject;
+    private $accessToken;
 
     public function __construct()
     {
@@ -26,30 +25,15 @@ class GoogleAuth
 
     /**
      * Get the access token
-     * @return string|null
+     * @return League\OAuth2\Client\Token\AccessToken|null
      */
     public function getAccessToken()
     {
-        $tokenObject = $this->getAccessTokenObject();
-        if (is_object($tokenObject) 
-            && $tokenObject instanceof AccessToken) {
-            return $tokenObject->accessToken;
-        }
-
-        return null;
-    }
-
-    /**
-     * Get the League\OAuth2\Client\Token\AccessToken
-     * @return League\OAuth2\Client\Token\AccessToken|null
-     */
-    private function getAccessTokenObject()
-    {
-        if (!$this->accessTokenObject) {
+        if (!$this->accessToken) {
             $code = Input::get('code');
             if ($code) {
                 try {
-                    $this->accessTokenObject = $this->provider->getAccessToken(
+                    $this->accessToken = $this->provider->getAccessToken(
                         'authorization_code', 
                         array('code' => $code)
                     );
@@ -57,7 +41,7 @@ class GoogleAuth
             }
         }
 
-        return $this->accessTokenObject;
+        return $this->accessToken;
     }
 
     /**
@@ -66,31 +50,27 @@ class GoogleAuth
      */
     public function callback()
     {
-        $code = Input::get('code');
         $user = null;
+        $token = $this->getAccessToken();
+        
+        if ($token) {
+            try {
+                $authUser = $this->provider->getUserDetails($token);
+                if ($authUser && $this->validateEmail($authUser->email)) {
+                    $user = new User;
+                    $user->uid         = $authUser->uid;
+                    $user->nickname    = $authUser->nickname;
+                    $user->fullName    = $authUser->fullName;
+                    $user->firstName   = $authUser->firstName;
+                    $user->lastName    = $authUser->lastName;
+                    $user->email       = $authUser->email;
+                    $user->location    = $authUser->location;
+                    $user->description = $authUser->description;
+                    $user->imageUrl    = $authUser->imageUrl;
+                    $user->urls        = $authUser->urls;
+                }
 
-        if ($code) {
-            $accessToken = $this->getAccessTokenObject();
-
-            if ($accessToken) {
-                try {
-                    $authUser = $this->provider->getUserDetails($accessToken);
-                    if ($authUser && $this->validateEmail($authUser->email)) {
-                        $user = new User;
-                        $user->uid         = $authUser->uid;
-                        $user->nickname    = $authUser->nickname;
-                        $user->fullName    = $authUser->fullName;
-                        $user->firstName   = $authUser->firstName;
-                        $user->lastName    = $authUser->lastName;
-                        $user->email       = $authUser->email;
-                        $user->location    = $authUser->location;
-                        $user->description = $authUser->description;
-                        $user->imageUrl    = $authUser->imageUrl;
-                        $user->urls        = $authUser->urls;
-                    }
-
-                } catch (Exception $e) {}
-            }
+            } catch (Exception $e) {}
         }
 
         return $user;
